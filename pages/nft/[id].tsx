7bb812266123bf103996da183bc14e91cea8108d
Norrismi <Norrismi0@gmail.com>
2022-10-28 19:43:15 -0400

    added web3

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { ConnectWallet, useAddress, useMetamask, useDisconnect } from "@thirdweb-dev/react";
 
 const NFTDropPage = () => {
+
+
+    // Auth
+    const connectWithMM = useMetamask();
+    const address = useAddress();
+    const disconnect = useDisconnect();
+
+    const [connectedAddress, setConnectedAddress] = useState(false)
+
+    console.log(address)
+
+
     return (
         <div className='flex h-screen flex-col lg:grid lg:grid-cols-10'>
 
@@ -31,15 +44,22 @@ const NFTDropPage = () => {
                         The <span className='font-extrabold underline decoration-pink-600/50'>Papa Fam</span> NFT Market Place
                     </h1>
 
-                    <button className="rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base">
-                        Sign In
-                    </button>
+                    <button onClick={() => (address ? disconnect() : connectWithMM()) } className="rounded-full bg-rose-400 text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base">
+                        { address ? 'Sign Out' : 'Sign In'}
+                    </button> 
                 </header>
 
+                <hr className='my-2 border' />
+                    <div className="mx-auto text-red-500/70">{address ? `Your logged in with wallet ${address.slice(0,5)}....${address.slice(-5)}` : null}</div>
 
-                <div className="content">
-                    Content
+
+                <div className="flex flex-col flex-1 items-center mt-10 space-y-6 text-center lg:justify-center lg:space-y-0 lg:justify-center">
+                    <img className='w-80 object-cover pb-10 lg:h-40' src="https://links.papareact.com/bdy" alt="pr2" />
+                    <h1 className="text-3xl font-bold lg:text-5xl lg:font-extrabold ">The PapaFam Ape coding Club</h1>
+                    <p className="pt-2 text-xl text-green-500 ">13/21 NFT's Claimed</p>
                 </div>
+
+                <button className="bg-red-600 h-16 w-full p-2 text-white rounded-full mt-10 font-bold">Mint NFT</button>
 
 
                 <div className="mint">
